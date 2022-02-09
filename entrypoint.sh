420f4666039fc5ffba19b6c2c437c49110140a8e
747lo <739769483@qq.com>
2022-02-09 11:00:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-echo AAA
-echo SECRET_TESTAA
-echo ${{ secrets.SECRET_TEST }}
-echo ---
-
-pwd
-ls -l
-cat a.txt
+#pwd
+#ls -l
+#cat a.txt
 
 
-cd /tmp
-pwd
-cat a.txt
+#cd /tmp
+#pwd
+#cat a.txt
